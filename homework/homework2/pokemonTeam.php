c97d84c3aa0e5d830320e7cb52e97e5c5914e485
Matthew Lisec <mlisec@csumb.edu>
2018-07-01 07:38:50 +0000

    homework files

--- homework/homework2/pokemonTeam.php
+++ homework/homework2/pokemonTeam.php
@@ -35,9 +35,89 @@
         
         function displayTeam(){
             global $team;
-            for ($i=0; $i < 6; $i++) {
-                echo $team[$i] . "<br>";
+            
+            foreach ($team as $a){
+                switch ($a){
+                    
+                    case 0: $name = "bulbasaur";//check
+                    
+                            break;
+                            
+                    case 1: $name = "squirtle";//check
+                    
+                            break;
+                            
+                    case 2: $name = "charmander";//check
+                    
+                            break;
+                            
+                    case 3: $name = "rockruff";//check
+                    
+                            break;
+                            
+                    case 4: $name = "weedle";//check
+                    
+                            break;
+                            
+                    case 5: $name = "dratini";//check
+                    
+                            break;
+                            
+                    case 6: $name = "vanillite";//check
+                    
+                            break;
+                            
+                    case 7: $name = "machop";//check
+                    
+                            break;
+                            
+                    case 8: $name = "pikipek";//check
+                    
+                            break;
+                            
+                    case 9: $name = "duskull";//check
+                    
+                            break;
+                            
+                    case 10: $name = "phanpy";//check
+                    
+                            break;
+                            
+                    case 11: $name = "shinx";//check
+                    
+                            break;
+                            
+                    case 12: $name = "lillipup";//check
+                    
+                            break;
+                            
+                    case 13: $name = "gulpin";//check
+                    
+                            break;
+                            
+                    case 14: $name = "abra";//check
+                    
+                            break;
+                            
+                    case 15: $name = "purrloin";//check
+                    
+                            break;
+                            
+                    case 16: $name = "klink";//check
+                    
+                            break;
+                            
+                    case 17: $name = "spritzee";//check
+                    
+                            break;
+                }
             }
+            
+            echo "<img src='img/$name.png' alt='$name' title='". ucfirst($name) . "'><br>";
+            
+            /*for ($i=0; $i < 6; $i++) {
+                echo $team[$i] . "<br>";
+            }*/
         
         }
         
