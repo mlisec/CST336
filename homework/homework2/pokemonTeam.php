da0c2781c27633986cf34a92c8a333a114b658b0
Matthew Lisec <mlisec@csumb.edu>
2018-07-01 06:31:43 +0000

    homework files

---
+++ homework/homework2/pokemonTeam.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <title>Pokemon Team</title>
+    </head>
+    
+    <body>
+        
+        <h1>Your team.</h1>
+        
+        <?php
+        
+        $team = array();
+        
+        function createTeam(){
+            while (count($team) < 6) {
+                $x = rand(0,17);
+                while (in_array($x, $team)) {
+                    $x = rand(0,17);
+                }
+                
+                array_push($team, $x);
+            }
+        }
+        
+        function displayTeam(){
+            
+            for ($i=0; $i < 6; $i++) {
+                echo $team[$i];
+            }
+        
+        }
+        
+        ?>
+        
+    </body>
+    
+</html>
