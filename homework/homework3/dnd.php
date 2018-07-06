5a2a9638c332c78bbbe1b19a3c47f062b03b8c65
Matthew Lisec <mlisec@csumb.edu>
2018-07-06 19:47:36 +0000

    hw 3

--- homework/homework3/dnd.php
+++ homework/homework3/dnd.php
@@ -7,29 +7,46 @@
     <body>
         <!-- name of character in a text box -->
         Enter your character name.
-        <input name="charName" type="text">
+        <form>
+            <input name="charName" type="text">
+        
         <br><br>
         
         <!-- radio for male or female -->
         Select your gender.
-        <form>
+        
             <input type="radio" name="gender" value="male"> Male<br>
             <input type="radio" name="gender" value="female"> Female<br>
             <input type="radio" name="gender" value="other"> Other  
-        </form>
+        
         <br><br>
         
         <!-- select for race -->
         Choose your race.
-        <select name="racelist" form="raceform">
-            <option value="human">Human</option>
-            <option value="dwarf">Dwarf</option>
-            <option value="elf">Elf</option>
-            <option value="halfling">Halfling</option>
-        </select>
+        
+            <select name="racelist" form="raceform">
+                <option value="human">Human</option>
+                <option value="dwarf">Dwarf</option>
+                <option value="elf">Elf</option>
+                <option value="halfling">Halfling</option>
+            </select>
+        
+        <br><br>
+        
         <!-- select for class -->
+        
+            <select name="classlist" form="classform">
+                <option value="cleric">Cleric</option>
+                <option value="fighter">Fighter</option>
+                <option value="rogue">Rogue</option>
+                <option value="wizard">wizard</option>
+            </select>
+        
         <!-- checkbox for starter items -->
         <!-- submit button generates starting stats -->
+        
+            <input type="submit" value="Submit" />
+        </form>
         
     </body>
 </html>
