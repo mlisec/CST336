757f22b3aa2330c0188d483bec9960174289f89f
Matthew Lisec <mlisec@csumb.edu>
2018-07-06 16:46:44 +0000

    hw 3

---
+++ homework/homework3/dnd.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>DnD Character</title>
+    </head>
+    
+    <body>
+        <!-- name of character in a text box -->
+        <input name="charName" type="text">
+        <!-- radio for male or female -->
+        <!-- select for race -->
+        <!-- select for class -->
+        <!-- checkbox for starter items -->
+        <!-- submit button generates starting stats -->
+        
+    </body>
+</html>
