691691fbf5908985570b9af5bd29eb74e7dadbf4
Matthew Lisec <mlisec@csumb.edu>
2018-07-06 16:56:35 +0000

    hw3

--- homework/homework3/dnd.php
+++ homework/homework3/dnd.php
@@ -6,10 +6,27 @@
     
     <body>
         <!-- name of character in a text box -->
-        Enter your chacter name.
+        Enter your character name.
         <input name="charName" type="text">
+        <br><br>
+        
         <!-- radio for male or female -->
+        Select your gender.
+        <form>
+            <input type="radio" name="gender" value="male"> Male<br>
+            <input type="radio" name="gender" value="female"> Female<br>
+            <input type="radio" name="gender" value="other"> Other  
+        </form>
+        <br><br>
+        
         <!-- select for race -->
+        Choose your race.
+        <select name="racelist" form="raceform">
+            <option value="human">Human</option>
+            <option value="dwarf">Dwarf</option>
+            <option value="elf">Elf</option>
+            <option value="halfling">Halfling</option>
+        </select>
         <!-- select for class -->
         <!-- checkbox for starter items -->
         <!-- submit button generates starting stats -->
