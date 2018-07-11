612d5cb68d785499b556de51c266062a0cb507a6
Matthew Lisec <mlisec@csumb.edu>
2018-07-11 03:00:52 +0000

    lab5

---
+++ labs/lab5/index.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE HTML>
+<html>
+    
+    <head>
+        <title> Ottermart Product Search </title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        
+        <div>
+            <h1> Ottermar Product Search </h1>
+            
+            <form>
+                
+                Product: <input type="text" name="product" />
+                
+                <br /><br />
+                <input type="submit" value="Search" name="searchFrom" />
+                
+            </form>
+            
+            <br />
+            
+        </div>
+        
+        <hr>
+        
+    </body>
+</html>
