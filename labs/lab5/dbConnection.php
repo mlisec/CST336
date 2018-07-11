adb918fba994acdf77cc918fd4858e64417d3513
Matthew Lisec <mlisec@csumb.edu>
2018-07-11 04:16:38 +0000

    lab5

--- labs/lab5/dbConnection.php
+++ labs/lab5/dbConnection.php
@@ -7,6 +7,15 @@ function getDatabaseConnection($dbname = 'ottermart') {
     $username = 'root';
     $password = '';
     
+    //when connecting from Heroku
+     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+
+     }
     //creates db connection
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     
