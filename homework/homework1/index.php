0dfbe6c72c9614c728a19ca511ddcc21375083b0
Matthew Lisec <mlisec@csumb.edu>
2018-08-10 04:39:50 +0000

    hw1

---
+++ homework/homework1/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+	    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+        <meta charset="utf-8">
+		<title>GAME DEVELOPMENT</title>
+		<link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body>
+        <header>
+			<h1>GAME DEVELOPMENT</h1>
+		</header>
+        <nav>
+			<a href="index.php"><span class="selectedPage">Home</span></a>
+			<a href="gamedevelopment.html">Game Development</a>
+			<a href="roles.html">Roles</a>
+			<a href="moredetail.html">More Detail</a>
+			<hr width="100%" />
+		</nav>
+		
+		<br /><br />
+		
+		<main>
+			
+				<figure id="homeImage">
+					<img src="img/controller.jpg" alt="Picture of ipad" />
+				</figure>
+				
+				<div id="homeText">
+				<p>
+				<br />The content of this website will be focusing on game development.
+				<br />Creating a video game seems to hold many people's interest because
+				<br />the end result will be a tangible product that the user can interact 
+				<br />with. But, what goes into making a video game? How long can it take?
+				<br />These are often the daunting questions early stage developers ask
+				<br />themselves when the idea of a game begins to form inside their head.
+				<br />
+				<br />We will go into more detail through this webpage.
+				<br />
+				<br />Move to the next page... 
+				<a href="gamedevelopment.html"><span class="nextpagelink">Game Development</span></a>
+				</p>
+				</div>
+				
+		</main>
+
+		
+        <footer>
+            <hr>
+			
+			<img src="img/logo.png" alt="CSUMB Logo" /><br />
+			
+			CSUMB CST 336. 2018&copy; Lisec <br />
+			<strong>Disclaimer:</strong> The information in this webpage is fictitious. <br />
+			It is used for academic purposes only.
+		</footer>
+        
+    </body>
+
+
+</html>
